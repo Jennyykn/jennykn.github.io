aa38c969024d6326be73a7242279f68ba8594b8c
Jennyykn <jennyykarlsson@gmail.com>
2020-11-18 20:28:55 -0700

    fixed better navigation with javascript

--- script/javaScript.js
+++ script/javaScript.js
@@ -1,11 +1,32 @@
-window.onscroll = function() {scrollPage()};
 
-function scrollPage() {
+function navFunc() {
     let navBar = document.getElementById("navBar");
+    let personInfo = document.getElementById("personalInfo");
+    let favBooks = document.getElementById("favBooks");
+    let haiku = document.getElementById("haiku");
+    let interests = document.getElementById("interests");
+    let schedule = document.getElementById("schedule");
+    
+    if (!navBar.classList.contains("stickyNav")) {
+        personInfo.style.cssText = "padding-top: 100px; margin-top: -100px";
+        favBooks.style.cssText = "padding-top: 100px; margin-top: -100px";
+        haiku.style.cssText = "padding-top: 100px; margin-top: -100px";
+        interests.style.cssText = "padding-top: 100px; margin-top: -100px";
+        schedule.style.cssText = "padding-top: 100px; margin-top: -100px";
+    
+    } else {
+        personInfo.style.cssText = "padding-top: 40px; margin-top: -40px";
+        favBooks.style.cssText = "padding-top: 40px; margin-top: -40px";
+        haiku.style.cssText = "padding-top: 40px; margin-top: -40px";
+        interests.style.cssText = "padding-top: 40px; margin-top: -40px";
+        schedule.style.cssText = "padding-top: 40px; margin-top: -40px";
+    }
+}
 
-
+window.addEventListener("scroll", function () {
+    let navBar = document.getElementById("navBar");
+    
     let stickyNavi = navBar.offsetTop;
-
 
     if (window.pageYOffset >= stickyNavi) {
         navBar.classList.add("stickyNav");
@@ -13,5 +34,5 @@ function scrollPage() {
     if (window.pageYOffset <= stickyNavi) {
         navBar.classList.remove("stickyNav");
     }
-}
+});
 

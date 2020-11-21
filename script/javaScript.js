79bb74657599ea9ac15fc028744518439110e02c
Jennyykn <jennyykarlsson@gmail.com>
2020-11-20 20:51:40 -0700

    final

--- script/javaScript.js
+++ script/javaScript.js
@@ -1,28 +1,4 @@
 
-function navFunc() {
-    let navBar = document.getElementById("navBar");
-    let personInfo = document.getElementById("personalInfo");
-    let favBooks = document.getElementById("favBooks");
-    let haiku = document.getElementById("haiku");
-    let interests = document.getElementById("interests");
-    let schedule = document.getElementById("schedule");
-    
-    if (!navBar.classList.contains("stickyNav")) {
-        personInfo.style.cssText = "padding-top: 100px; margin-top: -100px";
-        favBooks.style.cssText = "padding-top: 100px; margin-top: -100px";
-        haiku.style.cssText = "padding-top: 100px; margin-top: -100px";
-        interests.style.cssText = "padding-top: 100px; margin-top: -100px";
-        schedule.style.cssText = "padding-top: 100px; margin-top: -100px";
-    
-    } else {
-        personInfo.style.cssText = "padding-top: 40px; margin-top: -40px";
-        favBooks.style.cssText = "padding-top: 40px; margin-top: -40px";
-        haiku.style.cssText = "padding-top: 40px; margin-top: -40px";
-        interests.style.cssText = "padding-top: 40px; margin-top: -40px";
-        schedule.style.cssText = "padding-top: 40px; margin-top: -40px";
-    }
-}
-
 function modalView() {
     let modalMode = document.getElementById("viewPic");
     let image = document.getElementById("topPic");
